d20f286e845563ce449d10412a1fa39e2e87e770
rachelsprague <rachel@rachelsprague.com>
2025-05-23 13:48:07 -0700

    add script

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,9 +1,17 @@
-  document.addEventListener("DOMContentLoaded", function () {
-    const links = document.querySelectorAll("a[href^='http']");
-    links.forEach(link => {
-      if (!link.href.includes(window.location.hostname)) {
-        link.setAttribute("target", "_blank");
-        link.setAttribute("rel", "noopener noreferrer");
-      }
-    });
+// External links open in new tab
+document.addEventListener("DOMContentLoaded", function () {
+  const links = document.querySelectorAll("a[href^='http']");
+  links.forEach(link => {
+    if (!link.href.includes(window.location.hostname)) {
+      link.setAttribute("target", "_blank");
+      link.setAttribute("rel", "noopener noreferrer nofollow");
+    }
   });
+
+// Render obfuscated LinkedIn link
+  const linkedInElement = document.getElementById("js-linkedin");
+  if (linkedInElement) {
+    const profile = "rsprague";
+    linkedInElement.innerHTML = `<a href="https://www.linkedin.com/in/${profile}" rel="nofollow">LinkedIn</a>`;
+  }
+});
